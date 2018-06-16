afaabf13fea2b3fb27b362bdac12da2ef90924b3
Aurea-Li <fu.sakura@gmail.com>
2018-06-15 23:14:00 -0500

    readding safeway coupon code

--- coupon-clicking/Safeway.js
+++ coupon-clicking/Safeway.js
@@ -8,25 +8,64 @@
  * 
  *********************************************/
 
+/* Scroll to bottom of page */
+function scrollToBottom() {
+    while (window.scrollY + document.documentElement.clientHeight < document.body.scrollHeight) {
+        window.scrollTo(0, document.body.scrollHeight - document.documentElement.clientHeight);
+    } 
+    return;
+}
+scrollToBottom();
 
-var handle = setInterval(function () {checkButtons() },500);
+/* Click all coupon buttons */
+buttonList = document.querySelectorAll('span[role="button"]');
+
+
+for (var i = 0; i < buttonList.length; i++) {
+
+    (function (i) {
+        setTimeout(function () {
+            buttonList[i].click();
+            console.log('Coupon ' + i + ' clicked!');
+        }, 200 * i)
+    })(i);
+}
+
+
+
+
+let handle = setInterval(checkButtons,500);
+
+
+
+
+
+
+function stop() {
+
+    alert('hi');
+    if (numButtons == document.querySelectorAll('span[role="button"]').length) {
+        clearInterval(handle);
+        alert('Interval cleared');
+    }
+}
+
+window.onload = stop;
 
 function checkButtons() {
 
     let numButtons = document.querySelectorAll('span[role="button"]').length;
-
-    alert(numButtons);    
+  
     window.scrollTo(0, document.body.scrollHeight);
-    setTimeOut(){
-    
-        if (numButtons == document.querySelectorAll('span[role="button"]').length){
-            clearInterval(handle);
-            alert('Interval cleared');
-        }
-    }, 2000);
+
+
 }
 
 function stopInterval() {
     clearInterval(handle);
 }
+
+
+
+/* Temporary Code - Refreshes page and add buttons until stops */
 
