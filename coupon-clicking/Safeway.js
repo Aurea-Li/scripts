45a10492ad9efe43eeff026ad18e6b8542b5caee
Xin Huang <xyh1@users.noreply.github.com>
2019-04-13 08:43:21 -0700

    added additional functionality to load all
    site changed; need to click 'load more' button now

--- coupon-clicking/Safeway.js
+++ coupon-clicking/Safeway.js
@@ -8,8 +8,20 @@
  *
  *********************************************/
 
+/* Click button to load all coupons */
+
+for (i = 0; i < 20; i++) {
+  (function () {
+    setTimeout(function () {
+      clickmore = document.querySelector('button[class="btn load-more"]');
+      if (typeof clickmore != "undefined") {
+        clickmore.click();
+      }}, 300)  
+  })();
+}
 
 /* Click all coupon buttons */
+
 buttonList = document.querySelectorAll('span[role="button"]');
 
 for (var i = 0; i < buttonList.length; i++) {
